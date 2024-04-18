bba33bc2cac67999f1b7ce94870b5d8b50f763d2
Nick Perry <nick.perry12@outlook.com>
2024-04-17 18:25:39 -0700

    practice problems

--- EventsAndAsyncProblems/SelectionFilters.js
+++ EventsAndAsyncProblems/SelectionFilters.js
@@ -3,9 +3,8 @@
 // when the user chooses an option under Classifications, the items in the
 // Animals dropdown should change accordingly. Use the lookup tables below to
 // see which animals and classifications go together.
-
 const animalAndClass = {
-  Vertebrates: ['Bear', 'Turtle', 'Salmon', 'Whale', 'Ostrich'],
+  Vertebrate: ['Bear', 'Turtle', 'Salmon', 'Whale', 'Ostrich'],
   'Warm-blooded': ['Bear', 'Whale', 'Ostrich'],
   'Cold-blooded': ['Salmon', 'Turtle'],
   Mammal: ['Bear', 'Whale'],
@@ -16,9 +15,51 @@ const animals = {
   Bear: ['Vertebrate', 'Warm-blooded', 'Mammal'],
   Turtle: ['Vertebrate', 'Cold-blooded'],
   Whale: ['Vertebrate', 'Warm-blooded', 'Mammal'],
-  Salmnon: ['Vertebrate', 'Cold-blooded'],
+  Salmon: ['Vertebrate', 'Cold-blooded'],
   Ostrich: ['Vertebrate', 'Warm-blooded', 'Bird']
 }
 
-const animalClassifications = document.querySelector('#animal-classifications');
-const animalNames = document.querySelector('#animals');
+const animalClassifications = document.querySelector('#animal-classifications'); // classifications
+const animalNames = document.querySelector('#animals'); // animal list
+const clearBtn = document.querySelector('#clear');
+
+function addOptions(selectEle, options) {
+  options.forEach(option => {
+    selectEle.add(new Option(option, option));
+  })
+}
+
+function removeOptions(selectElement) {
+  while (selectElement.options.length > 0) {
+    selectElement.remove(0);
+  }
+}
+
+function setDefaults() {
+  removeOptions[animalClassifications];
+  removeOptions[animalNames];
+  
+  animalClassifications.add(new Option('Classification', 'Classification'));
+  animalNames.add(new Option('Animals', 'Animals'));
+
+  addOptions(animalClassifications, defaultOptions['Classifications']);
+  addOptions(animalNames, defaultOptions['Animals']);
+}
+
+animalClassifications.addEventListener("change", event => {
+  let classification = event.target.value;
+  removeOptions(animalNames);
+  addOptions(animalNames, animalAndClass[classification]);
+});
+
+animalNames.addEventListener("change", event => {
+  event.stopPropagation();
+  let animal = event.target.value;
+  removeOptions(animalClassifications);
+  addOptions(animalClassifications, animals[animal]);
+});
+
+clearBtn.addEventListener('click', event => {
+  event.preventDefault();
+  setDefaults();
+});
